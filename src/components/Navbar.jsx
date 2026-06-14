fb7d552aad5bbac8c3f3ef7131d4114d0c3ab987
Piyush Kumar Jha <piyushkumarjha@Piyushs-MacBook-Air.local>
2026-06-14 23:12:59 +0530

    Day 1 completed - Navbar and Hero Section

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import "../styles/Navbar.css";
+
+function Navbar() {
+  return (
+    <nav>
+      <h2>ShopSphere</h2>
+
+      <ul>
+        <li>Home</li>
+        <li>Products</li>
+        <li>Cart</li>
+        <li>Login</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import "../styles/Home.css";
+
+function Home() {
+  return (
+    <div className="hero">
+      <h1>Welcome to ShopSphere</h1>
+      <p>Your one-stop online shopping destination.</p>
+      <button>Shop Now</button>
+    </div>
+  );
+}
+
+export default Home;
